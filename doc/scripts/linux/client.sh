ecabe5c302ea4ff87f4b2842b28ebbbf1a587c8d
raduchis <radu.chis@gmail.com>
2015-05-28 10:55:18 -0700

    Fixed problems with the starting of clients and server 	Added log4j from maven repository 	Automatically copying the jars in the lib folder to target 	Added client.sh and server.sh for starting the client and server from bash

---
+++ doc/scripts/linux/client.sh
@@ -0,0 +1,1 @@
+java -cp "fadse-0.0.1-SNAPSHOT.jar:dependency-jars/*" ro.ulbsibiu.fadse.Boot client
---
+++ doc/scripts/linux/server.sh
@@ -0,0 +1,1 @@
+java -cp "fadse-0.0.1-SNAPSHOT.jar:dependency-jars/*" ro.ulbsibiu.fadse.Boot my_config_sniper.xml 1> output 2>&1
